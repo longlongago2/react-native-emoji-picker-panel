7c0064a12e20238eadd877bcf8e9d4817212a108
longlongago2 <1789136583@qq.com>
2017-10-31 17:36:08 +0800

    支持prop-types

--- RenderTabBar.js
+++ RenderTabBar.js
@@ -1,66 +1,86 @@
-import React from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
-  StyleSheet,
-  View,
-  TouchableOpacity,
-  Text
-} from 'react-native'
-
-const FacebookTabBar = React.createClass({
-  tabIcons: [],
-
-  propTypes: {
-    goToPage: React.PropTypes.func,
-    activeTab: React.PropTypes.number,
-    tabs: React.PropTypes.array
-  },
-
-  render () {
-    const tabUnderlineStyle = {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      height: 2,
-      backgroundColor: '#007aff',
-      bottom: 0
-    }
-    const tabLineStyle = {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      height: 2,
-      backgroundColor: 'rgba(0,0,0,0.05)',
-      bottom: 0
-    }
-    return <View style={[styles.tabs, this.props.style]}>
-      {this.props.tabs.map((tab, i) => {
-        return (
-          <TouchableOpacity activeOpacity={0.7} key={tab} onPress={() => this.props.goToPage(i)} style={styles.tab}>
-            <Text style={styles.text}>{tab}</Text>
-            {this.props.activeTab === i ? <View style={tabUnderlineStyle} /> : <View style={tabLineStyle} />}
-          </TouchableOpacity>
-        )
-      })}
-    </View>
-  }
-})
+    StyleSheet,
+    View,
+    TouchableOpacity,
+    Text,
+} from 'react-native';
 
 const styles = StyleSheet.create({
-  tab: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingBottom: 10
-  },
-  tabs: {
-    height: 45,
-    flexDirection: 'row',
-    paddingTop: 5
-  },
-  text: {
-    fontSize: 20,
-    color: 'black'
-  }
-})
+    tab: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: 10,
+    },
+    tabs: {
+        height: 45,
+        flexDirection: 'row',
+        paddingTop: 5,
+    },
+    text: {
+        fontSize: 20,
+        color: 'black',
+    },
+});
 
-export default FacebookTabBar
+class FacebookTabBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tabIcons: [],
+        };
+    }
+
+    static propTypes = {
+        goToPage: PropTypes.func,
+        activeTab: PropTypes.number,
+        tabs: PropTypes.array,
+    };
+
+    render() {
+        const tabUnderlineStyle = {
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            height: 2,
+            backgroundColor: '#007aff',
+            bottom: 0,
+        };
+        const tabLineStyle = {
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            height: 2,
+            backgroundColor: 'rgba(0,0,0,0.05)',
+            bottom: 0,
+        };
+        const { style, tabs, goToPage, activeTab } = this.props;
+        return (
+            <View style={[styles.tabs, style]}>
+                {
+                    tabs.map((tab, i) => {
+                        return (
+                            <TouchableOpacity
+                                activeOpacity={0.7}
+                                key={tab}
+                                onPress={() => goToPage(i)}
+                                style={styles.tab}
+                            >
+                                <Text style={styles.text}>{tab}</Text>
+                                {
+                                    activeTab === i ?
+                                        <View style={tabUnderlineStyle} /> :
+                                        <View style={tabLineStyle} />
+                                }
+                            </TouchableOpacity>
+                        );
+                    })
+                }
+            </View>
+        );
+    }
+}
+
+export default FacebookTabBar;
--- example/index.android.js
+++ example/index.android.js
@@ -4,22 +4,34 @@
  * @flow
  */
 
-import React, { Component } from 'react'
-import {
-  AppRegistry
-} from 'react-native'
-import EmojiPicker from 'react-native-emoji-picker-panel'
-export default class emoji extends Component {
-  _emojiSelected (emoji) {
-    console.log(emoji)
-  }
+import React, { Component } from 'react';
+import { AppRegistry, View, Text } from 'react-native';
+import EmojiPicker from 'react-native-emoji-picker-panel';
 
-  render () {
-    return (
-      <EmojiPicker
-        onEmojiSelected={this._emojiSelected.bind(this)} />
-    )
-  }
+export default class Emoji extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            emoji: '',
+        };
+        this.handleEmojiSelected = this._handleEmojiSelected.bind(this);
+    }
+
+    _handleEmojiSelected(emoji) {
+        this.setState({ emoji });
+    }
+
+    render() {
+        const { emoji } = this.state;
+        return (
+            <View>
+                <Text>{emoji}</Text>
+                <EmojiPicker
+                    onEmojiSelected={this.handleEmojiSelected}
+                />
+            </View>
+        );
+    }
 }
 
-AppRegistry.registerComponent('emoji', () => emoji)
+AppRegistry.registerComponent('emoji', () => Emoji);
